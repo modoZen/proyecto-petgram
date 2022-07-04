192d32cbeef2aefbdddf20e954cc82b4270ea137
Max <mdiazn@uni.pe>
2022-07-03 22:34:55 -0500

    instalacion_react-babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.warn('El comienzo!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Slava Stalin!</h1>,
+ document.getElementById('app'));
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,28 @@ module.exports = {
     filename: 'app.js'
   },
   plugins:[
-    new HtmlWebpackPlugin(),
-  ]
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+  ],
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: 'babel-loader',
+          options:{
+            presets:[
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            plugins:[
+              "@babel/plugin-transform-runtime"
+            ]
+          }
+        }
+      }
+    ]
+  }
 }

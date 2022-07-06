1800fc4420ff945ad87d930769784b5632d06107
Max <mdiazn@uni.pe>
2022-07-06 16:28:37 -0500

    useCategoriesData en ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,21 +2,29 @@ import React, { useEffect, useState } from "react";
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = ()=>{
-  const [categories, setCategories ] = useState([]);
-  const [showFixed, setShowFixed ] = useState(false);
+function useCategoriesData() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading ] = useState(false);
   useEffect(()=>{
     async function getData(){
-      const response = await fetch('https://petgram-server-max-seven.vercel.app/categories');
-      const data = await response.json();
+      setLoading(true);
+      const response  = await fetch('https://petgram-server-max-seven.vercel.app/categories');
+      const data      = await response.json();
       setCategories(data);
+      setLoading(false);
     }
     getData();
-  },[]);
+  },[])
+  return { categories, loading }
+}
+
+
+export const ListOfCategories = ()=>{
+  const { categories, loading } = useCategoriesData();
+  const [ showFixed, setShowFixed ] = useState(false);
   useEffect(()=>{
     const onScroll = e => {
       const newShowFixed = window.scrollY>200
-      console.log(window.screenY)
       showFixed !== newShowFixed &&
       setShowFixed(newShowFixed)
     }
@@ -28,13 +36,16 @@ export const ListOfCategories = ()=>{
     }
   },[showFixed]);
 
-  const renderList = (fixed = false)=>(
+  const renderList = (fixed)=>(
     <List className={fixed?'fixed':''}>
       {
-        categories.map(category=><Item key={category.id}><Category {...category} /></Item>)
+      loading?
+      <Item key='loading'><Category /></Item>:
+      categories.map(category=><Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
+
   return (
     <>
       {renderList()}

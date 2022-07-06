c011a808bc822576b3670a793925b8a58bcc1166
Max <mdiazn@uni.pe>
2022-07-06 12:09:11 -0500

    ListOfPhotoCards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { GlobalStyle } from './GlobalStyles';
-import { PhotoCard } from './components/PhotoCard';
+import { ListOfPhotoCards } from './components/ListOfPhotoCards';
 
 const App = ()=>{
   return (
     <>
       <GlobalStyle />
       <ListOfCategories />
-      <PhotoCard />
+      <ListOfPhotoCards />
     </>
   );
 }

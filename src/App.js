1c9f15f99e3bef6641fe5904e348630535cd9b3e
Max <mdiazn@uni.pe>
2022-07-06 11:43:44 -0500

    PhotoCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { GlobalStyle } from './GlobalStyles';
+import { PhotoCard } from './components/PhotoCard';
 
 const App = ()=>{
   return (
     <>
       <GlobalStyle />
       <ListOfCategories />
+      <PhotoCard />
     </>
   );
 }

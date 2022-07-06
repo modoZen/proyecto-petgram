11ee95bcdb05a97f6eccfd88caa5717c293653ac
Max <mdiazn@uni.pe>
2022-07-06 12:32:18 -0500

    Logo SVGR

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { GlobalStyle } from './GlobalStyles';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { Logo } from './components/Logo'
 
 const App = ()=>{
   return (
     <>
+      <Logo />
       <GlobalStyle />
       <ListOfCategories />
       <ListOfPhotoCards />

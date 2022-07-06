a8ea4d3f98e20325b6baa5a4091459c8ad502af7
Max <mdiazn@uni.pe>
2022-07-06 13:21:49 -0500

    animaciones fadeInkeyframes

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { fadeIn } from "../../styles/animation";
+
 
 export const ImgWrapper = styled.div`
     border-radius: 10px;
@@ -10,8 +12,8 @@ export const ImgWrapper = styled.div`
     width: 100%;
     margin-top: 10px
 `
-
 export const Img = styled.img`
+  ${fadeIn({time:'5s'})}
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
   height: 100%;
   object-fit: cover;

467295a4de241d56267b8f49be5c73ea6974eb80
Max <mdiazn@uni.pe>
2022-07-07 16:06:48 -0500

    Estilando las páginas activas

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { MdHome, MdFavoriteBorder, MdPersonOutline } from "react-icons/md";
+import { useLocation } from "react-router-dom";
 import { Link, Nav } from "./styles";
 
 const SIZE = '32px';
 
 export const NavBar = () =>{
+  const { pathname } = useLocation();
+  console.info(pathname)
   return (
     <Nav>
-      <Link to="/" ><MdHome size={SIZE} /></Link>
-      <Link to="/favs" ><MdFavoriteBorder size={SIZE} /></Link>
-      <Link to="/user" ><MdPersonOutline size={SIZE} /></Link>
+      <Link to="/" className={pathname==='/'?'selected':''} ><MdHome size={SIZE} /></Link>
+      <Link to="/favs" className={pathname==='/favs'?'selected':''}  ><MdFavoriteBorder size={SIZE} /></Link>
+      <Link to="/user" className={pathname==='/user'?'selected':''}  ><MdPersonOutline size={SIZE} /></Link>
     </Nav>
   )
 }

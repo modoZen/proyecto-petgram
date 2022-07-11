ced2ee4ae017be0a5a8481cd1b17ca072ff63cfa
Max <mdiazn@uni.pe>
2022-07-11 13:30:54 -0500

    PropTypes para validar las props

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdFavoriteBorder } from "react-icons/md";
-import { Button, LikeIcon } from "./styles"
+import { Button, LikeIcon } from "./styles";
+import PropTypes from 'prop-types'
 
 export const FavButton = ({onClick, likes, liked}) =>{
 
@@ -11,4 +12,10 @@ export const FavButton = ({onClick, likes, liked}) =>{
       <Icon size='32px' /> {likes} likes!
     </Button>
   )
+}
+
+FavButton.propTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 }
--- src/components/SubmitButton/index.js
+++ src/components/SubmitButton/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Button } from './styles'
+import PropTypes from 'prop-types'
 
 export const SubmitButton = ({onClick, text, disabled = false})=>{
   return (
     <Button disabled={disabled} onClick={onClick} >{text}</Button> 
   )
+}
+
+SubmitButton.propTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  text: PropTypes.node.isRequired
 }
